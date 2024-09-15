2e3d9caaafffba1a2a502625ac2a4af2158976f2
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-09-14 18:58:49 -0600

    Using seeders for first time. I watch until 29:57 in the cap 7

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,19 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('style.css')}}">
 </head>
 <body>
+    <div class="container">
     @forelse ($products as $product )
-    <div>
+    <div class="card">
         <h3>{{ $product->name }}</h3>
         <p>{{ $product->short_description }}</p>
-        <p>{{ $product->price }}</p>
+        <p>{{ $product->price }} USD</p>
     </div>
 
     @empty
     <h5>No Data</h5>
 
     @endforelse
+</div>
 
 </body>
 </html>
